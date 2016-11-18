fe7c72885db19b6823f86eb423ed77f0c9d66fe8
matheus92 <lima.eca@gmail.com>
2016-11-18 16:24:55 -0200

    testing database

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ mongo.connect(link, function(err, db) {
         if(err) throw err;
         var urls = db.collection('urls');
         urls.find({
-            fullURL: {'www.google.com'}
+            fullURL: 'www.google.com'
         }).toArray(function(err, docs){
             if(err) throw err;
             console.log(docs);
