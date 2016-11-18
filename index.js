b81c66cb91feac77c04b71b3178c4b9171244ec0
matheus92 <lima.eca@gmail.com>
2016-11-18 17:15:26 -0200

    testing database

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var mongo = require('mongodb').MongoClient;
 
 var link = 'mongodb://Matheus:kirkhetfield92@ds157247.mlab.com:57247/urls';
 
+
+/*
 mongo.connect(link, function(err, db) {
         if(err) throw err;
         var urls = db.collection('urls');
-        urls.find({
+        urls.findOne({
             fullURL: { $eq: 'www.google.com' }
         },{
             name: 1,
@@ -18,5 +20,28 @@ mongo.connect(link, function(err, db) {
             }
             console.log(docs);
             db.close();
+        });
 });
+*/
+
+mongo.connect(link, function(err, db) {
+    if(err) {
+        console.log("Erro aqui!");
+        throw err;
+    }
+    var urls = db.collection('urls');
+    urls.findOne({
+        fullURL: { $eq: 'www.google.com' }
+    }, function(error, result){
+        if (error){ 
+            console.log("Deu erro!!")
+        }
+        if (result){
+            console.log("Encontrou algo!!");
+        }
+        else{
+            console.log("Nao encontrou nada!!");
+        }
+
+    });
 });
