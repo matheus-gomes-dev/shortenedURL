2027303f14f26cdce6f15239b616519e9ff40eeb
matheus92 <lima.eca@gmail.com>
2016-11-18 22:28:08 -0200

    testing url

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
+var express = require('express');
+var app = express();
 var mongo = require('mongodb').MongoClient;
 
-var link = 'mongodb://Matheus:kirkhetfield92@ds157247.mlab.com:57247/urls';
+
+var link = 'mongodb://Matheus:testingdb1234@ds157247.mlab.com:57247/urls';
+
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('*', function(req, response) {  
+  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl; 
+  var parameter = (req.originalUrl).substring(1,(req.originalUrl).length);
+  console.log(fullUrl);
+  console.log(parameter);
+
+
+});
+
 
 
 /*
-mongo.connect(link, function(err, db) {
-        if(err) throw err;
-        var urls = db.collection('urls');
-        urls.findOne({
-            fullURL: { $eq: 'www.google.com' }
-        },{
-            name: 1,
-            age: 1,
-            _id: 0
-        }).toArray(function(err, docs){
-            if(err){
-                console.log("Entrou A");
-                throw err;
-            }
-            console.log(docs);
-            db.close();
-        });
-});
-*/
-
-
 
 mongo.connect(link, function(err, db) {
     if(err) {
@@ -48,7 +49,7 @@ mongo.connect(link, function(err, db) {
     });
 });
 
-
+*/
 
 
 
